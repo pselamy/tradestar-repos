2fe9876bf61be7948f1cb982f20b5fcee1c6e41a
Patrick Selamy <pselamy@gmail.com>
2022-04-09 14:05:42 -0500

    Add `instrument_repository`.

---
+++ src/main/java/com/verlumen/tradestar/repositories/instruments/InstrumentRepository.java
@@ -0,0 +1,10 @@
+package com.verlumen.tradestar.candles.repositories;
+
+
+import com.verlumen.tradestar.protos.instruments.Instrument;
+
+import java.time.Instant;
+
+public interface InstrumentRepository {
+    Instrument get(String code);
+}

c35b91afb3c859fd65189301e99a58edb61cfff6
Patrick Selamy <pselamy@gmail.com>
2022-04-09 14:41:44 -0500

    Remove redundant import.

--- src/main/java/com/verlumen/tradestar/repositories/instruments/InstrumentRepository.java
+++ src/main/java/com/verlumen/tradestar/repositories/instruments/InstrumentRepository.java
@@ -3,8 +3,6 @@ package com.verlumen.tradestar.repositories.instruments;
 
 import com.verlumen.tradestar.protos.instruments.Instrument;
 
-import java.time.Instant;
-
 public interface InstrumentRepository {
     Instrument getInstrument(String symbol);
 }

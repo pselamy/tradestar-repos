f82f58d60275a3399ae9577128d8c0a8b33714a2
Patrick Selamy <pselamy@gmail.com>
2022-04-10 16:09:16 -0500

    Init `AlphaVantage`.

--- src/main/java/com/verlumen/tradestar/repositories/candles/AlphaVantageCandleRepository.java
+++ src/main/java/com/verlumen/tradestar/repositories/candles/AlphaVantageCandleRepository.java
@@ -1,7 +1,10 @@
 package com.verlumen.tradestar.repositories.candles;
 
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
+import com.google.inject.Inject;
 import com.verlumen.tradestar.protos.candles.Candle;
 import com.verlumen.tradestar.protos.candles.Granularity;
 import com.verlumen.tradestar.protos.instruments.Instrument;
@@ -9,6 +12,12 @@ import com.verlumen.tradestar.protos.instruments.Instrument;
 import java.time.Instant;
 
 class AlphaVantageCandleRepository implements CandleRepository {
+
+    @Inject
+    AlphaVantageCandleRepository(Config config) {
+        AlphaVantage.api().init(config);
+    }
+
     @Override
     public ImmutableSet<Candle> getCandles(Instrument instrument, Granularity granularity, Range<Instant> timeRange) {
         throw new UnsupportedOperationException();
